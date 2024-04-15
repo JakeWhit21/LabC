b253fdda330ea820556990313bc0411650452dad
Jake <jdwhitmore3@gmail.com>
2024-04-14 23:43:15 -0500

    Add project files.

---
+++ Morse/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Morse
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Morse/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace Morse
+{
+    public partial class MainPage : ContentPage
+    {
+        private string dotsNDashes;
+        private string message;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            dotsNDashes = "";
+            message = "";
+
+        }
+
+        private void Dash_Clicked(object sender, EventArgs e)
+        {
+            dotsNDashes += "-";
+            DotsAndDashes.Text = dotsNDashes;
+        }
+
+        private void Dot_Clicked(object sender, EventArgs e)
+        {
+            dotsNDashes += ".";
+            DotsAndDashes.Text = dotsNDashes;
+        }
+
+        private void Space_Clicked(object sender, EventArgs e)
+        {
+            char letter = Morse.MorseCoder(dotsNDashes);
+            message += letter;
+            Letters.Text = message;
+            dotsNDashes = "";
+        }
+
+        
+
+
+    }
+
+}
